f720c5b2f703b1bb42347bfe289b54e537d76aa5
Pomogach <122118431+Pomogachoff@users.noreply.github.com>
2023-01-17 16:33:58 +0500

    Update special.js

--- special.js
+++ special.js
@@ -1,6 +1,9 @@
-/* ТУТ параметр отвечаешь за ширину поля ввода */
-let ChatFooter = document.querySelector(`#chat-footer`);
-let ChatMainc000001Width = document.querySelector(`#chat-main-c000001`).offsetWidth;
-let ChatFooterWidth = ChatMainc000001Width;
 
-ChatFooter.style.width = ChatFooterWidth;
+function CardImgHeight(){
+let CardImg = document.querySelector(`#no_avatar`);
+let CardTextHeight = document.querySelector(`#text-chat`).Height;
+
+CardImg.style.height = `${CardTextHeight}` + `px`;
+}; 
+
+let timerId2 = setInterval(CardImgHeight, 100);
